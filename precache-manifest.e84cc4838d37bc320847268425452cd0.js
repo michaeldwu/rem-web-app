d8ee482c783953f9d1a574f5ca8d793b9b4e0070
Nikhil <nikhilbehari@college.harvard.edu>
2020-05-02 14:01:20 -0400

    Updates

---
+++ precache-manifest.e84cc4838d37bc320847268425452cd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0257dc97e33f8007b10cbcdccd918444",
+    "url": "/rem-web-app/index.html"
+  },
+  {
+    "revision": "abf2c759e03a7b0c5883",
+    "url": "/rem-web-app/static/css/main.562bdbaf.chunk.css"
+  },
+  {
+    "revision": "dcaaa6ef8b21b557fd28",
+    "url": "/rem-web-app/static/js/2.a7a095d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rem-web-app/static/js/2.a7a095d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf2c759e03a7b0c5883",
+    "url": "/rem-web-app/static/js/main.80244998.chunk.js"
+  },
+  {
+    "revision": "6364e14935cc44ebd7f2",
+    "url": "/rem-web-app/static/js/runtime-main.2107b80f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rem-web-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
