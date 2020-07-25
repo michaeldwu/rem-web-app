2bd0e0ad1d428876ec47463ee3d3fd44433a5b93
Nikhil <nikhilbehari@gmail.com>
2020-07-24 20:20:59 -0400

    Launch Page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,91 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
-import blank_image from './resources/blank_image.png';
+// Custom Components
+import bg from './resources/bg_blur.jpg';
+import logo from './resources/rem_logo.png';
+import { SocialIcon } from 'react-social-icons';
 
-import DonateForm from './components/DonateForm.js'
-import './App.css';
-import {
-  useLocation
-} from "react-router-dom";
+const styles = theme => ({
+  root: {
+    height: '100vh',
+    width: '100%',
+    backgroundFilter: 'blur(1000px)',
+    backgroundImage: "url("+bg+")",
+    backgroundSize: 'cover'
+  },
+  main: {
+    paddingBottom: '180px',
+  },
+  social: {
+    display: 'flex',  
+    justifyContent:'center', 
+    alignItems:'center',
+  },
+  title: {
+    display: 'flex',  
+    justifyContent:'center', 
+    alignItems:'center',
+    paddingTop: '40px',
+    fontFamily: 'Lato',
+    fontWeight: '700',
+    fontSize: 40,
+    color: 'white'
+  },
+  logo: {
+    width: '50px',
+    paddingRight: '10px'
+  },
+  message: {
+    display: 'flex',  
+    justifyContent:'center', 
+    alignItems:'center',
+    paddingTop: '200px',
+    fontFamily: 'Lato',
+    fontSize: 60,
+    color: 'white',
+    whiteSpace: 'nowrap',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 30,
+    },
+  },
+  copyRight: {
+    display: 'flex',  
+    justifyContent:'center', 
+    alignItems:'center',
+    fontSize: 10,
+    color: 'white'
+  }
+});
 
-
-// A custom hook that builds on useLocation to parse
-// the query string for you.
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
-
-function getDescription(){
-  return "Put brief description of charity here"
-}
-
-function App() {
-  let query = useQuery();
+function App(props) {
+  const { classes } = props;
   return (
-    <div className="App">
-        <header className="App-header">
-          <div className="Image-container">
-            <img src={blank_image} className="Background-image" alt="bimg" />
+    <div className={classes.root}>
+
+        <div className={classes.main}>
+          <div className={classes.title}>
+            <img src={logo} className={classes.logo} alt="logo" />
+            rem
           </div>
-        </header>
-      <DonateForm charityName={query.get("name")} charityDescription={getDescription()} charityAmount={query.get("amount")}/>
+
+          <div className={classes.message}>
+            Launching August 24th
+          </div>
+        </div>
+        <hr style={{width:'80%'}}/>
+
+        <div className={classes.social}>
+          <SocialIcon bgColor='white' url="https://www.instagram.com/rem.fyc" style={{margin: '30px'}}/>
+          <SocialIcon bgColor='white' url="https://www.youtube.com/channel/UCglok9Hll2SF2PsDfqVCkzA" style={{margin: '30px'}} />
+        </div>
+
+        <div className={classes.copyRight}>
+          Copyright © 2020 rem - All Rights Reserved.
+        </div>
+
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/pages/Donate.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import blank_image from './resources/blank_image.png';
+
+import DonateForm from './components/DonateForm.js'
+import './App.css';
+import {
+  useLocation
+} from "react-router-dom";
+
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function getDescription(){
+  return "Put brief description of charity here"
+}
+
+function Donate() {
+  let query = useQuery();
+  return (
+    <div className="App">
+        <header className="App-header">
+          <div className="Image-container">
+            <img src={blank_image} className="Background-image" alt="bimg" />
+          </div>
+        </header>
+      <DonateForm charityName={query.get("name")} charityDescription={getDescription()} charityAmount={query.get("amount")}/>
+    </div>
+  );
+}
+
+export default Donate;
